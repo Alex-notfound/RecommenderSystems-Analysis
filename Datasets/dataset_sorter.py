79add9288056a025b6170ab9d3fe5e5d6ac03153
Alexandre Currás Rodríguez <alexcurrasr@gmail.com>
2020-09-02 03:43:00 +0200

    Update dataset_sorter.py
    Now each way of sorting is on a function.
    Added a random sorter.

--- Datasets/dataset_sorter.py
+++ Datasets/dataset_sorter.py
@@ -1,16 +1,32 @@
-import sys, csv, operator
+import sys, csv, operator, random
 
-data = csv.reader(open('dataset_out.csv', 'r'),delimiter=';')
+def sort_by_user():
+    data = csv.reader(open('dataset_out.csv', 'r'),delimiter=';')
+    sortedlist = sorted(data, key=operator.itemgetter(0))    # 0 specifies according to first column we want to sort
+    with open("dataset_OrderByUser.csv", "w", newline='') as f:
+        fileWriter = csv.writer(f, delimiter=';')
+        for row in sortedlist:
+            fileWriter.writerow(row)
 
-sortedlist = sorted(data, key=operator.itemgetter(0))    # 0 specifies according to first column we want to sort
-with open("dataset_OrderByUser.csv", "w", newline='') as f:
-    fileWriter = csv.writer(f, delimiter=';')
-    for row in sortedlist:
-        fileWriter.writerow(row)
+def sort_by_item():
+    data = csv.reader(open('dataset_out.csv', 'r'),delimiter=';')
+    sortedlist = sorted(data, key=operator.itemgetter(1))
+    with open("dataset_OrderByItem.csv", "w", newline='') as f:
+        fileWriter = csv.writer(f, delimiter=';')
+        for row in sortedlist:
+            fileWriter.writerow(row)
 
-data = csv.reader(open('dataset_out.csv', 'r'),delimiter=';')
-sortedlist2 = sorted(data, key=operator.itemgetter(1))
-with open("dataset_OrderByItem.csv", "w", newline='') as f2:
-    fileWriter2 = csv.writer(f2, delimiter=';')
-    for row in sortedlist2:
-        fileWriter2.writerow(row)
+def sort_random():
+    fi = open('dataset_out.csv', 'r')
+    r = fi.readlines()
+    fi.close()
+
+    random.shuffle(r)
+
+    fo = open('dataset_shuffle.csv', 'w')
+    fo.writelines(r)
+    fo.close()
+
+sort_by_user()
+sort_by_item()
+sort_random()
